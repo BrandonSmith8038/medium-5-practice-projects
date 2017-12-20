8e73f0621edaab92a2295cab58ed9050df292f67
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-19 19:14:19 -0700

    Mobile Nav Menu, some mobile styling

---
+++ one-page-responsive/main.js
@@ -0,0 +1,38 @@
+console.log('Main.js Loaded')
+
+//================
+//Sticky Na
+//================
+
+//When the user scrolls the page, execute my function
+window.onscroll = function () {
+  myFunction()
+}
+
+//Get The Navebar 
+const navbar = document.querySelector('.nav')
+
+//Get The offset position of the navbar
+const sticky = navbar.offsetTop
+
+//Add the sticky class to the navbar when you reach its scroll position. Remove sticky when the leave scroll position.
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky')
+  }
+}
+
+//================
+//Sticky Na
+//================
+
+const navigation = document.getElementById('navigation')
+
+const hamButton = document.querySelector('.toggle-nav')
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open')
+})
