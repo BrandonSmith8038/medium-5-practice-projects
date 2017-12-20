aedad80240e69d16fe62c5b8d7d6d4d08c20c0b7
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-20 03:13:18 -0700

    Hero Section

--- multi-page-travel/gulpfile.js
+++ multi-page-travel/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
   return gulp
     .src('*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./css'));
 });
 
