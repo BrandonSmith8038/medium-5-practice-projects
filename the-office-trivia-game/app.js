20beab36d12e2523e2954f40e3ef5b91c447b768
Brandon Smith <brandon@reddirtwebdesign.com>
2018-01-07 14:05:27 -0700

    Clean Up App.js

--- the-office-trivia-game/app.js
+++ the-office-trivia-game/app.js
@@ -4,13 +4,6 @@ const ui = new UI();
 //Init Game Class
 const game = new Game();
 
-/* const gameArea = document.getElementById('game');
-const questionArea = document.getElementById('questionArea');
-
-let questions = [];
-//let currentQuestion = 0;
-let correctAnswer = ''; */
-
 function gameStart() {
   game.getQuestions().then(questions => {
     game.askQuestion();
@@ -21,22 +14,7 @@ function gameStart() {
 
 function getRandomQuestion() {
   // const randomQuestion = Math.floor(Math.random() * (questions.length - 1 + 1));
-
   return randomQuestion;
-}
-
-function debug() {
-  console.log('***************Debug******************');
-  console.clear();
-  console.log('Questions', questions);
-  console.log(('Current Question', randNum));
-  console.log('Question', questions[randNum].question);
-  console.log('Possible Answers', questions[randNum].possbileAnswers);
-  console.log('Correct Answer', correctAnswer);
-  console.log('Number Correct', game.getScore());
-  console.log('Random Max', questions.length);
-  console.log('Randon Number', randNum);
-  console.log('***************Debug******************');
 }
 
 gameStart();
